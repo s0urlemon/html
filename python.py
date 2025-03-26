656937c301e9a91ca2e4754015d5aa03bcea1d07
s0urlemon <dheekshida.balaji@gmail.com>
2025-03-26 17:32:01 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,5 @@
+print("(5+3)/4=",(5+3)/4)
+print("This question uses BODMAS to help solve it")
+#using a  hashtag helps us write a comment
+print("BODMAS is an acronym to help children remember the order of mathematical operations – the correct order in which to solve maths problems")
+#using print("") we can print the data
